f4a62da89453420dc7534e33e802bc12a140a4cf
VeslRuben <itzRuben96@gmail.com>
2019-10-16 16:20:39 +0200

    fikst aduino code so it can read from web server and added python veb server

--- Arduino/wifi-module/wifi-module.ino
+++ Arduino/wifi-module/wifi-module.ino
@@ -1,55 +1,52 @@
 #include <WiFi.h>
- 
+
 const char* ssid = "RD";
 const char* password =  "11111111";
- 
+
 const uint16_t port = 6969;
 const char * host = "192.168.43.120";
- 
+
 void setup()
 {
- 
+
   Serial.begin(115200);
- 
+
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("...");
   }
- 
+
   Serial.print("WiFi connected with IP: ");
   Serial.println(WiFi.localIP());
- 
+
 }
- 
+
 void loop()
 {
-    WiFiClient client;
- 
-    if (!client.connect(host, port)) {
- 
-        Serial.println("Connection to host failed");
- 
-        delay(1000);
-        return;
-    }
- 
-    Serial.println("Connected to server successful!");
- 
-    client.print("Hello from ESP32!");
+  WiFiClient client;
 
-    while(millis() < 10000) {
-      byte y = client.read();
-      if(y != -1) {
-        Serial.println(y);
-      }
-    }
-    Serial.println(client.read());
- 
-    Serial.println("Disconnecting...");
+  if (!client.connect(host, port)) {
 
-    
-    client.stop();
- 
-    delay(10000);
+    Serial.println("Connection to host failed");
+
+    delay(1000);
+    return;
+  }
+
+  Serial.println("Connected to server successful!");
+
+  client.print("Hello from ESP32!");
+
+  while (millis() < 10000) {
+    String y = client.readStringUntil('\n');
+    Serial.println(y);
+  }
+
+  Serial.println("Disconnecting...");
+
+
+  client.stop();
+
+  delay(10000);
 }
