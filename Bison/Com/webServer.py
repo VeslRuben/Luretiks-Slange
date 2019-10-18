e3d4406947277d4c17e93fe563e9cf80633db304
VeslRuben <itzRuben96@gmail.com>
2019-10-18 16:29:02 +0200

    addet send metode to the server

--- Bison/Com/webServer.py
+++ Bison/Com/webServer.py
@@ -27,72 +27,77 @@ class Server:
             self.corruptedDataCounter = 0
             self.startTime = time.time()
 
-    def receive(self) -> list:
-        data = None
-        try:
-            messageType, addr = self.sock.recvfrom(10024)  # buffer size is 10024 bytes
+    def send(self, data: str):
+        print("sending")
+        self.sock.sendto(data.encode(), ("192.168.137.113", 9696))
 
-            if messageType.decode() == "a":
-                print("Type: ", messageType)
-                data = self.parse2dArray()
-
-            self.startTime2 = time.time()
-
-        except socket.timeout as e:
-            pass
-        except CorruptedDataError as e:
-            self.corruptedDataCounter += 1
-        except TimeoutError as e:
-            pass
-
-        self.checkConection()
-        if time.time() - self.startTime2 > 5:
-            raise TimeoutError("No data receded in 5 seconds")
-
-        return data
-
-    def parse2dArray(self) -> list:
-        startTime = time.time()
-        while True:
+        def receive(self) -> list:
+            data = None
             try:
-                header, addr = self.sock.recvfrom(10024)  # buffer size is 10024 bytes
+                messageType, addr = self.sock.recvfrom(10024)  # buffer size is 10024 bytes
 
-                if len(header) < 3:  # Header is to short, data probably lost.
-                    raise CorruptedDataError("header receded is corrupt")
+                if messageType.decode() == "a":
+                    print("Type: ", messageType)
+                    data = self.parse2dArray()
+                elif messageType.decode() == "t":
+                    pass
 
-                x = int(header[0])
-                y = int(header[2])
-                size = x * y
-                print("Header: ", header)
+                self.startTime2 = time.time()
 
-                if time.time() - startTime > 0.5:  # Timeout
-                    raise TimeoutError("timed out wn receding header")
-                break
             except socket.timeout as e:
                 pass
+            except CorruptedDataError as e:
+                self.corruptedDataCounter += 1
+            except TimeoutError as e:
+                pass
 
-        startTime = time.time()
-        data = b""
-        while True:
-            try:
-                dataPart, addr = self.sock.recvfrom(10024)  # buffer size is 10024 bytes
-                data += dataPart
-                if len(data) == x * y:  # Reseved all data
+            # self.checkConection()
+            # if time.time() - self.startTime2 > 5:
+            #   raise TimeoutError("No data receded in 5 seconds")
+
+            return data
+
+        def parse2dArray(self) -> list:
+            startTime = time.time()
+            while True:
+                try:
+                    header, addr = self.sock.recvfrom(10024)  # buffer size is 10024 bytes
+
+                    if len(header) < 3:  # Header is to short, data probably lost.
+                        raise CorruptedDataError("header receded is corrupt")
+
+                    x = int(header[0])
+                    y = int(header[2])
+                    size = x * y
+                    print("Header: ", header)
+
+                    if time.time() - startTime > 0.5:  # Timeout
+                        raise TimeoutError("timed out wn receding header")
                     break
-                if time.time() - startTime > 0.5:  # Timeout
-                    raise TimeoutError("timed out wn receding data")
-            except socket.timeout as e:
-                pass
+                except socket.timeout as e:
+                    pass
 
-        array = []
-        i = 0
-        for x in range(100):
-            temp = []
-            for y in range(200):
-                temp.append(data[i])
-            array.append(temp)
-        return array
+            startTime = time.time()
+            data = b""
+            while True:
+                try:
+                    dataPart, addr = self.sock.recvfrom(10024)  # buffer size is 10024 bytes
+                    data += dataPart
+                    if len(data) == x * y:  # Reseved all data
+                        break
+                    if time.time() - startTime > 0.5:  # Timeout
+                        raise TimeoutError("timed out wn receding data")
+                except socket.timeout as e:
+                    pass
 
+            array = []
+            i = 0
+            for x in range(100):
+                temp = []
+                for y in range(200):
+                    temp.append(data[i])
+                array.append(temp)
+            return array
 
-if __name__ == '__main__':
-    s = Server()
+    if __name__ == '__main__':
+        s = Server()
--- Bison/main.py
+++ Bison/main.py
@@ -5,7 +5,6 @@ from Bison.Com.webServer import Server
 server = Server()
 
 while True:
-    data = server.receive()
-    print(data)
-    if data != None:
-        picture = np.array(data)
+    inn = input("-> ")
+
+    server.send(inn)
