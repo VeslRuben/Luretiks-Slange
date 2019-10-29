52bb569a47122b091a8bcbddc3d135f27a2ca9dc
GryfinOlai <marcusgrindvik@gmail.com>
2019-10-29 13:53:14 +0100

    Rotated the picture in videostream. And finished the Find target class, only need some Cleaning up. FindSnake class is made, but noting is implimented

---
+++ Bison/ImageProcessing/findSnake.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+
+class FindSnake:
+
+    def __init__(self):
+
+        pass
+
+    def LocateSnake(self):
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    c = FindSnake()
+    c.LocateSnake()
+
